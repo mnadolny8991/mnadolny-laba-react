e4a572992f2c0bdb2d7a6ca29cdac00d4366bd3a
Michał Nadolny <mnadolny.laba@solvd.com>
2024-04-13 14:49:51 +0200

    fix: add btn image change after changing theme

--- homeworks/css/index.js
+++ homeworks/css/index.js
@@ -1,16 +1,14 @@
-document.querySelector('.btn-switch').addEventListener('click', changeTheme);
+let btn = document.querySelector('.btn-switch');
+btn.addEventListener('click', changeTheme);
 
 function changeTheme() {
     const body = document.querySelector('body');
     let currentTheme = body.classList[0];
     if (currentTheme === 'light-theme') {
         body.classList.replace('light-theme', 'dark-theme');
-        console.log('should change');
-        console.log(body.classList[0]);
+        document.querySelector('.btn-switch__img').src = "./assets/theme-icon-dark.svg";
     } else {
         body.classList.replace('dark-theme', 'light-theme');
-        console.log('should change');
-        console.log(body.classList[0]);
+        document.querySelector('.btn-switch__img').src = "./assets/theme-icon-light.svg";
     }
-    console.log(body.classList[0]);
 }
