e506c9489dd1c3bfec937235c42aa95c1d5278e3
Michał Nadolny <mnadolny.laba@solvd.com>
2024-05-23 10:50:55 +0200

    feat: add correctness tests

--- homeworks/data-structures-and-algorithms/solution.js
+++ homeworks/data-structures-and-algorithms/solution.js
@@ -16,9 +16,9 @@ function linearSearchBySku(sku, data) {
 // at the cost of additional memory usage + creation time required for storing a sorted data
 
 // COMPLEXITY ANALYSIS
+// O(log(n)) - search time
 // Theta(nlogn) - index creation average case | O(n^2) - worst case (very rare when using quick sort)
 // O(n) - additional memory usage
-// O(log(n)) - search time
 class ItemSkuIndex {
     constructor(data, sort) {
         this._sortedData = sort(data, (a, b) => {
@@ -74,10 +74,49 @@ function sort(data, cmpFunc) {
         qs(arr, left, partitionIdx - 1);
         qs(arr, partitionIdx + 1, right);
     }
-    data = [...data];
+    data = structuredClone(data);
     qs(data, 0, data.length - 1);
     return data;
 }
+
+// CORRECTNESS TESTS
+// If this code fails then unhadled error is thrown and the progam won't create a log
+function testCorrectness() {
+    let item;
+    [item, comp] = linearSearchBySku('', data);
+    if (item)
+        throw new Error('linear search is not working');
+    [item, comp] = linearSearchBySku('0dd6bb31-3be6-40ed-8b97-51c33d0154d9', data);
+    if (!item || item.name !== 'Salmon - Smoked, Sliced')
+        throw new Error('linear search is not working');
+    [item, comp] = linearSearchBySku('979c2fba-aabc-4772-a03d-876448f2db6a', data);
+    if (!item || item.name !== 'Sobe - Green Tea')
+        throw new Error('linear search is not working');
+    [item, comp] = linearSearchBySku('7980dcfb-fa04-4c60-a3a4-2892f83146c7', data);
+    if (!item || item.price !== '£15.42')
+        throw new Error('linear search is not working');
+    [item, comp] = linearSearchBySku('c15fb538-7284-44f9-8417-fcab49cbb4f3', data);
+    if (!item || item.pack !== 15)
+        throw new Error('linear search is not working');
+
+    const idx = new ItemSkuIndex(data, sort);
+    [item, comp] = idx.find('');
+    if (item) 
+        throw new Error('linear search is not working');
+    [item, comp] = idx.find('0dd6bb31-3be6-40ed-8b97-51c33d0154d9');
+    if (!item || item.name !== 'Salmon - Smoked, Sliced')
+        throw new Error('linear search is not working');
+    [item, comp] = idx.find('979c2fba-aabc-4772-a03d-876448f2db6a');
+    if (!item || item.name !== 'Sobe - Green Tea')
+        throw new Error('linear search is not working');
+    [item, comp] = idx.find('7980dcfb-fa04-4c60-a3a4-2892f83146c7');
+    if (!item || item.price !== '£15.42')
+        throw new Error('linear search is not working');
+    [item, comp] = idx.find('c15fb538-7284-44f9-8417-fcab49cbb4f3');
+    if (!item || item.pack !== 15)
+        throw new Error('linear search is not working');
+}
+testCorrectness();
 
 // PERFORMANCE TESTS
 const EXPERIMENT_COUNT = 1000000;
@@ -100,6 +139,9 @@ const needleList = [
     'd462bb76-81ee-46af-9fdb-ebfe53a93d3f',
     '6df55f86-e3f5-4d7b-9cd5-906d8d7e804a',
     '1e63459f-0b18-4acf-9afc-e7287347bbeb',
+    'ccdb70f4-91f1-4543-93fa-8a93f980dc99',
+    '5ce1e0a6-1e39-406e-b2be-08bce24b33b3',
+    '0dd6bb31-3be6-40ed-8b97-51c33d0154d9',
 ];
 
 function getRandomSku() {
