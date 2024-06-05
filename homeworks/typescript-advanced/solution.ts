ef7b34291619983e21610b7224840772dab6bb44
Michał Nadolny <mnadolny.laba@solvd.com>
2024-06-05 09:31:10 +0200

    feat: add BasicCalculator class

--- homeworks/typescript-advanced/solution.ts
+++ homeworks/typescript-advanced/solution.ts
@@ -8,7 +8,31 @@ interface Calculator {
     subtract(a: number, b: number): Result;
     multiply(a: number, b: number): Result;
     divide(a: number, b: number): Result;
-    power(a: number, b: number): Result;
     power(base: number, exponent: number): Result;
     sqrt(num: number): Result;
+}
+
+class BasicCalculator implements Calculator {
+    add(a: number, b: number): Result {
+        return { value: a + b, error: '' };
+    }
+    subtract(a: number, b: number): Result {
+        return { value: a - b, error: '' };
+    }
+    multiply(a: number, b: number): Result {
+        return { value: a * b, error: '' };
+    }
+    divide(a: number, b: number): Result {
+        return { value: a / b, error: '' };
+    }
+    power(base: number, exponent: number): Result {
+        if (!Number.isInteger(exponent) || exponent < 0) 
+            return { value: NaN, error: 'Exponent must be a positive integer' };
+        return { value: Math.pow(base, exponent), error: '' };
+    }
+    sqrt(num: number): Result {
+        if (num < 0)
+            return { value: NaN, error: 'Cannot calculate the square root of a negative number' };
+        return { value: Math.sqrt(num), error: '' }
+    }
 }
