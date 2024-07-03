256e8c5c3f140f2713f9f7b64b03c247f62d85de
Michał Nadolny <mnadolny.laba@solvd.com>
2024-07-03 11:12:57 +0200

    feat: add proper lights colors

--- homeworks/react-basics/index.jsx
+++ homeworks/react-basics/index.jsx
@@ -15,9 +15,9 @@ function TrafficLights({ color }) {
     <div className="wrapper">
         <LightsContainer>
             <Head />
-            <Light color="red"/>
-            <Light />
-            <Light />
+            <Light color={color === 'red' && '#DF4040'}/>
+            <Light color={color === 'yellow' && '#E9EC6A'}/>
+            <Light color={color === 'green' && '#04CA00'}/>
         </LightsContainer>
     </div>);
 }
@@ -25,4 +25,4 @@ function TrafficLights({ color }) {
 const element2 = <h1>Hello From JSX</h1>;
 const domContainer = document.querySelector('#root_container');
 const root = ReactDOM.createRoot(domContainer);
-root.render(<TrafficLights color='green' />);
+root.render(<TrafficLights color='red' />);
