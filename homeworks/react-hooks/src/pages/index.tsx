f04ed046837d4420dec715cf407b12ed30c5f34d
Michał Nadolny <mnadolny.laba@solvd.com>
2024-07-12 11:05:14 +0200

    feat: add task done checkbox

--- homeworks/react-hooks/src/pages/index.tsx
+++ homeworks/react-hooks/src/pages/index.tsx
@@ -28,15 +28,6 @@ function TodoApp() {
   const [tasks, setTasks] = useState(new Array<Task>());
 
   useEffect(() => {
-    // const loadedTasks = [];
-    // for (const key of Object.keys(localStorage)) {
-    //   if (key !== 'ally-supports-cache') {
-    //     const desc = localStorage.getItem(key);
-    //     if (desc)
-    //       loadedTasks.push({ id: key, description: desc} );
-    //   }
-    // }
-    // setTasks(loadedTasks);
     const tasksStr = localStorage.getItem('tasks');
     if (!tasksStr) return;
     const data: Array<Task> = JSON.parse(tasksStr);
@@ -44,9 +35,6 @@ function TodoApp() {
   }, []);
 
   function handleTaskSubmit(taskDescription: string) {
-    // const id = uuidv4();
-    // setTasks([{ description: taskDescription, id }, ...tasks]);
-    // localStorage.setItem(id, taskDescription);
     const id = uuidv4();
     const task: Task = { description: taskDescription, id, done: false };
     const newTasks = [task, ...tasks];
@@ -61,7 +49,13 @@ function TodoApp() {
   } 
 
   function handleTaskChange(id: string, newDescription: string) {
-    const newTasks = tasks.map(t => t.id === id ? { id: id, description: newDescription, done: t.done } : t);
+    const newTasks = tasks.map(t => t.id === id ? { id: id, description: newDescription, done: false } : t);
+    setTasks(newTasks);
+    localStorage.setItem('tasks', JSON.stringify(newTasks));
+  }
+
+  function handleTaskDone(id: string, done: boolean) {
+    const newTasks = tasks.map(t => t.id === id ? { id: id, description: t.description, done } : t);
     setTasks(newTasks);
     localStorage.setItem('tasks', JSON.stringify(newTasks));
   }
@@ -75,8 +69,10 @@ function TodoApp() {
             <Task 
               key={t.id} 
               taskDescription={t.description} 
+              taskDone={t.done}
               onTaskDelete={() => handleTaskDelete(t.id)}
-              onTaskChange={(newDescription: string) => handleTaskChange(t.id, newDescription)}/>
+              onTaskChange={(newDescription: string) => handleTaskChange(t.id, newDescription)}
+              onTaskDone={(done: boolean) => handleTaskDone(t.id, done)}/>
           )}
         </ul>
       </div>
@@ -113,48 +109,63 @@ function TaskForm({ onTaskSubmit }: { onTaskSubmit: (desc: string) => void }) {
   );
 }
 
-function Task({ taskDescription, onTaskDelete, onTaskChange }: 
+function Task({ taskDescription, taskDone, onTaskDelete, onTaskChange, onTaskDone }: 
   { 
     taskDescription: string, 
+    taskDone: boolean,
     onTaskDelete: () => void, 
-    onTaskChange: (newDescription: string) => void 
+    onTaskChange: (newDescription: string) => void,
+    onTaskDone: (done: boolean) => void 
   }) {
   const [disabled, setDisabled] = useState(true);
   const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+
+  }, []);
 
   useEffect(() => {
     inputRef.current?.focus();
   }, [disabled]);
 
   return (
-    <div className={styles['task']}>
-      <input 
-        type="text" 
-        disabled={disabled} 
-        className={styles['task__input']}
-        onChange={(e: any) => {
-          onTaskChange(e.target.value);
-        }}
-        value={taskDescription}
-        ref={inputRef}
-        maxLength={30}>
-      </input>
-      <button className={styles['task__btn']} onClick={() => setDisabled(!disabled)}>
-        <Image
-          width={63.23}
-          height={65}
-          src="/write.svg"
-          alt="write image"
-        />
-      </button>
-      <button className={styles['task__btn']} onClick={onTaskDelete}>
-        <Image
-          width={59.38}
-          height={65}
-          src="/delete.svg"
-          alt="write image"
-        />
-      </button>
+    <div>
+      <div className={styles['complete-checkbox']}>
+        <input type="checkbox" id="completed" checked={taskDone} onChange={(e: any) => {
+          const checked = e.target.checked;
+          onTaskDone(checked);
+        }}></input>
+        <label htmlFor="completed">Mark as Completed</label>
+      </div>
+      <div className={styles['task']}>
+        <input 
+          type="text" 
+          disabled={disabled} 
+          className={`${styles['task__input']} ${taskDone && styles['task__input_done']}`}
+          onChange={(e: any) => {
+            onTaskChange(e.target.value);
+          }}
+          value={taskDescription}
+          ref={inputRef}
+          maxLength={30}>
+        </input>
+        <button className={styles['task__btn']} onClick={() => setDisabled(!disabled)}>
+          <Image
+            width={63.23}
+            height={65}
+            src="/write.svg"
+            alt="write image"
+          />
+        </button>
+        <button className={styles['task__btn']} onClick={onTaskDelete}>
+          <Image
+            width={59.38}
+            height={65}
+            src="/delete.svg"
+            alt="write image"
+          />
+        </button>
+      </div>
     </div>
   );
 }
